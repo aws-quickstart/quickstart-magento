43c6e67bdaf29e644b86ef39f88701a02b87d850
Scott Kellish <skellish@amazon.com>
2017-09-08 16:09:35 -0400

    updated to magento 2.1.8

---
+++ ci/usemaster.sh
@@ -0,0 +1,6 @@
+mv config.yml config.yml.local
+mv config.yml.master config.yml
+mv quickstart-magento-aurora1.json quickstart-magento-aurora1.json.local
+mv quickstart-magento-aurora1.json.master quickstart-magento-aurora1.json
+mv quickstart-magento-mysqlt1.json quickstart-magento-mysqlt1.json.local
+mv quickstart-magento-mysqlt1.json.master quickstart-magento-mysqlt1.json
