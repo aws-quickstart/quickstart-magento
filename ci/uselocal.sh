d6f77df819c31e8859b8f92d197bee9b9fcad5c6
Scott Kellish <skellish@amazon.com>
2017-09-25 12:23:39 -0400

    Added support for Aurora

--- ci/uselocal.sh
+++
@@ -1,6 +0,0 @@
-mv config.yml config.yml.master
-mv config.yml.local config.yml
-mv quickstart-magento-aurora1.json quickstart-magento-aurora1.json.master
-mv quickstart-magento-aurora1.json.local quickstart-magento-aurora1.json
-mv quickstart-magento-mysqlt1.json quickstart-magento-mysqlt1.json.master
-mv quickstart-magento-mysqlt1.json.local quickstart-magento-mysqlt1.json
